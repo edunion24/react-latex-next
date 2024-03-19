d94e5b1b7c346228608db57d6309026a1f0b3ff3
hanbitkim <hanbit2.kim@gmail.com>
2024-03-19 16:44:46 +0900

    add trust option

--- src/Latex.tsx
+++ src/Latex.tsx
@@ -1,26 +1,42 @@
-import * as React from 'react'
+import * as React from "react";
 
-import renderLatex, { Macros } from './renderLatex';
-import { Delimiter } from './types';
-import './Latex.css'
+import renderLatex, { Macros } from "./renderLatex";
+import { Delimiter } from "./types";
+import "./Latex.css";
 
 export interface LatexProps {
   children: string | string[];
   delimiters?: Delimiter[];
   strict?: boolean;
-  macros?: Macros
+  trust?: boolean;
+  macros?: Macros;
 }
 
 const defaultDelimiters = [
-  { left: '$$', right: '$$', display: true },
-  { left: '\\(', right: '\\)', display: false },
-  { left: '$', right: '$', display: false },
-  { left: '\\[', right: '\\]', display: true },
-]
+  { left: "$$", right: "$$", display: true },
+  { left: "\\(", right: "\\)", display: false },
+  { left: "$", right: "$", display: false },
+  { left: "\\[", right: "\\]", display: true },
+];
 
-export default function Latex({children, delimiters = defaultDelimiters, strict = false, macros }: LatexProps) {
-  const renderedLatex = renderLatex(Array.isArray(children) ? children.join('') : children, delimiters!, strict!, macros);
+export default function Latex({
+  children,
+  delimiters = defaultDelimiters,
+  strict = false,
+  macros,
+  trust,
+}: LatexProps) {
+  const renderedLatex = renderLatex(
+    Array.isArray(children) ? children.join("") : children,
+    delimiters!,
+    strict!,
+    macros,
+    trust
+  );
   return (
-    <span className="__Latex__" dangerouslySetInnerHTML={{ __html: renderedLatex }} />
-  )
+    <span
+      className="__Latex__"
+      dangerouslySetInnerHTML={{ __html: renderedLatex }}
+    />
+  );
 }
--- src/renderLatex.ts
+++ src/renderLatex.ts
@@ -1,23 +1,33 @@
 /** Adapted from /contrib/auto-render/auto-render.js at github.com/Khan/KaTeX */
 
-import { renderToString } from 'katex';
-import { Delimiter } from './types';
-import splitAtDelimiters from './splitAtDelimiters';
+import * as katex from "katex";
+import { Delimiter } from "./types";
+import splitAtDelimiters from "./splitAtDelimiters";
 
 export type Macros = { [name: string]: string };
 
-export default function renderLatexInTextAsHTMLString(text: string, delimiters: Delimiter[], strict: boolean, macros?: Macros): string {
+export default function renderLatexInTextAsHTMLString(
+  text: string,
+  delimiters: Delimiter[],
+  strict: boolean,
+  macros?: Macros,
+  trust?: boolean
+): string {
   const data = splitAtDelimiters(text, delimiters);
-  const fragments = []
+  const fragments = [];
 
   for (let i = 0; i < data.length; i++) {
-    if (data[i].type === 'text') {
+    if (data[i].type === "text") {
       fragments.push(data[i].data);
     } else {
       const latex = data[i].data;
       const displayMode = data[i].display;
       try {
-        const rendered = renderToString(latex, { displayMode, macros });
+        const rendered = katex.renderToString(latex, {
+          displayMode,
+          macros,
+          trust,
+        });
         fragments.push(rendered);
       } catch (error) {
         if (strict) {
@@ -28,5 +38,5 @@ export default function renderLatexInTextAsHTMLString(text: string, delimiters: 
     }
   }
 
-  return fragments.join('');
-};
+  return fragments.join("");
+}
